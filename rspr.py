8b8eb7480029b293397fd90bf7f703fdb206fd37
jaunzo <jabaunzo@yahoo.com>
2021-02-10 20:42:33 +1300

    Added pairwise rspr function

--- rspr.py
+++ rspr.py
@@ -34,7 +34,6 @@ def rspr(tree1, tree2):
     Runs an external executable that calculates the rspr of 2 binary
     phylogenetic trees
     """
-
     input_string = tree1 + "\n" + tree2
     
     if platform.system() == "Windows":
@@ -55,19 +54,62 @@ def rspr(tree1, tree2):
     
     length = len(output_list)
     
-    
     forest = output_list[length - 3]
     distance = output_list[-1].split()
     distance = distance[-1]
     equals_index = distance.index("=")
-    distance = int(distance[equals_index+1:])
+    distance = distance[equals_index+1:]
     
     clusters = forest.split()[2:]
     
     return (distance, clusters)
+
+
+def rspr_pairwise(trees):
+    """
+    Takes a list of trees and runs rspr for every pair of trees
+    """
+    length = len(trees)
     
+    distance_array = [["-" for i in range(length)] for j in range(length)]
+    clusters_array = [["-" for i in range(length)] for j in range(length)]
+    
+    for i in range(len(trees)):
+        for j in range(i, len(trees)):
+            (distance, clusters) = rspr(trees[i], trees[j])
+            distance_array[i][j] = distance
+            clusters_array[i][j] = clusters
+            
+    return (distance_array, clusters_array)
+
     
 if __name__ == "__main__":
-    tree1 = "(((1,2),3),4);"
-    tree2 = "(((1,4),2),3);"
-    print(rspr(tree1, tree2))
+    trees = []
+    trees.append("(((((((1,9),2),((13,3),8)),12),15),(((((14,6),4),7),11),10)),5);")
+    trees.append("((((((((((14,6),4),7),11),(1,9)),2),((13,3),8)),15),(10,12)),5);")
+    trees.append("(((((((14,6),4),7),11),(10,12)),((((1,9),2),((13,3),8)),15)),5);")
+    trees.append("((((((((((14,6),4),7),11),((1,5),9)),2),((13,3),8)),12),15),10);")
+    trees.append("(((((((1,5),9),2),((13,3),8)),12),15),(((((14,6),4),7),11),10));")
+    
+    (distances, clusters) = rspr_pairwise(trees)
+    
+    length = len(distances)
+
+    
+    for i in range(length):
+        print(", ".join(distances[i]))
+        
+    print()
+        
+    for i in range(length-1):
+        print(f"Clusters compared with t{i+1}:")
+        for j in range(i+1, len(clusters[i])):
+            print(f"t{j+1} (drSPR = {distances[i][j]}): {' '.join(clusters[i][j])}")
+            
+        print()
+        
+        
+    
+    
+    
+    
